f51cb9ab00fe213e09722af8f78cce9f0ceeee01
YC <yashdipsite@gmail.com>
2019-05-18 17:40:42 +0530

    Updated comments

--- Trees/Print BST elements in given range.java
+++ Trees/Print BST elements in given range.java
@@ -1,7 +1,12 @@
 void printNearNodes(Node node, int k1, int k2)
     {
+        //Similar to inorder traversal
+        //Only traverse to subtree within range and print elements within range
         if(node==null) return;
         //If current node is greater than k1, move to left subtree
+        //As long as k1 is less than current node, move left
+        //Once current becomes less than k1, start searching larger elements 
+        //And print 
         if(node.key>k1) printNearNodes(node.left,k1,k2);
         //If current node is within range
         if(node.key>=k1&&node.key<=k2) System.out.print(node.key+" ");
