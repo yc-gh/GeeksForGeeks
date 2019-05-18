855dea3634ea2ad03a7b40ef4c38c380ee51e0a8
YC <yashdipsite@gmail.com>
2019-05-18 12:53:50 +0530

    Initial commit

---
+++ Trees/Symmetric Tree.java
@@ -0,0 +1,19 @@
+public static boolean isSymmetric(Node root)
+    {
+        if(root==null)
+        return true;
+        //Need to check the left and right subtrees of root
+        //Split them and check if they mirror each other
+        return check(root.left,root.right);
+    }
+    public static boolean check(Node a, Node b)
+    {
+        //If both trees are traversed without any mismatch
+        if(a==null&&b==null) return true;
+        //One is null other is not
+        if(a==null||b==null) return false;
+        //Data is not equal
+        if(a.data!=b.data) return false;
+        //Recursively travel and check (in a mirrored fashion)
+        return check(a.left,b.right)&&check(a.right,b.left);
+    }
