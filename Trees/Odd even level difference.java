fdb396bce71dec44d4de7423a0edc6da7104496b
YC <yashdipsite@gmail.com>
2019-05-18 23:21:45 +0530

    Modified

--- Trees/Odd even level difference.java
+++ Trees/Odd even level difference.java
@@ -1,36 +1,5 @@
 int getLevelDiff(Node root)
     {
-        if (root == null) 
-            return; 
-  
-  
-        Stack<Node> s = new Stack<Node>(); 
-        Node curr = root; 
-  
-        // traverse the tree 
-        while (curr != null || s.size() > 0) 
-        { 
-  
-            /* Reach the left most Node of the 
-            curr Node */
-            while (curr !=  null) 
-            { 
-                /* place pointer to a tree node on 
-                   the stack before traversing 
-                  the node's left subtree */
-                s.push(curr); 
-                curr = curr.left; 
-            } 
-  
-            /* Current must be NULL at this point */
-            curr = s.pop(); 
-  
-            System.out.print(curr.data + " "); 
-  
-            /* we have visited the node and its 
-               left subtree.  Now, it's right 
-               subtree's turn */
-            curr = curr.right; 
-        }
-        return 0;
+        if(root==null) return 0;
+        return root.data - getLevelDiff(root.left) - getLevelDiff(root.right);
     }
