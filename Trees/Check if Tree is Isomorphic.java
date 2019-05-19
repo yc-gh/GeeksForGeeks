ac4eb5b0f4db504cfe590bbfa8a30c0344c5731a
yc-gh <36479105+yc-gh@users.noreply.github.com>
2019-05-19 13:36:50 +0530

    Update Check if Tree is Isomorphic.java

--- Trees/Check if Tree is Isomorphic.java
+++ Trees/Check if Tree is Isomorphic.java
@@ -9,7 +9,7 @@ public boolean isIsomorphic(Node root1,Node root2)
         if(root1.data!=root2.data) return false;
         //Check persistent or inverted combinations betweeen left and right subtrees of each
         boolean persist = isIsomorphic(root1.left,root2.left)&&isIsomorphic(root1.right,root2.right);
-	    boolean invert = isIsomorphic(root1.left,root2.right)&&isIsomorphic(root1.right,root2.left);
+	boolean invert = isIsomorphic(root1.left,root2.right)&&isIsomorphic(root1.right,root2.left);
         //Return true if either combination yields true(Isomorphic)
         return persist||invert;
     }
