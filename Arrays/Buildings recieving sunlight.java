bd7fe957ed55cb4ada1bda1002d94bdeac49104c
YC <yashdipsite@gmail.com>
2019-06-10 11:48:29 +0530

    Initial commit

--- Arrays/Buildings recieving sunlight.java
+++ Arrays/Buildings recieving sunlight.java
@@ -4,8 +4,10 @@ public static void main (String[] args) throws IOException{
 		while(t-->0)
 		{
 		    int n = Integer.parseInt(br.readLine().trim());
-		    String[] input = br.readLine().trim().split("\\s+");
-		    int maxheight = Integer.parseInt(input[0]);
+			String[] input = br.readLine().trim().split("\\s+");
+			//Iterate through array
+			//Only those buildings will recieve light which have more height than building with max height prior to them
+			int maxheight = Integer.parseInt(input[0]);
 		    int count=1;
 		    for(int i=1;i<n;i++)
 		    {
--- Arrays/Count distinct elements in every window.java
+++ Arrays/Count distinct elements in every window.java
@@ -1,6 +1,7 @@
 void countDistinct(int A[], int k, int n)
     {
-        StringBuilder output = new StringBuilder();
+		StringBuilder output = new StringBuilder();
+		//Using a sliding window, add k elements to a set and check size of set (distinct elements) 
 	    for(int i=0;i<n-(k-1);i++)
 	    {
 	        Set<Integer> set = new HashSet<>();
