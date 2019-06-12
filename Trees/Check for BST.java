c6a758d6457c7236af6b731b3bc4daeb2bcc8e11
YC <yashdipsite@gmail.com>
2019-06-12 19:03:27 +0530

    Initial commit

--- Trees/Check for BST.java
+++ Trees/Check for BST.java
@@ -1,6 +1,8 @@
 //Inefficient approach -- 
 //for each node check if left subtree's max value is less than node's data and right subtree's min value is greater than node's data
 
+//Alternative approach -- Check if inorder traversal is sorted
+
 //Efficient approach -- create a helper method that contains min and max values possible for current node
 //For each node, check if it is within that range
 //For it's left subtree, the range becomes previousmin <-> node.data-1
