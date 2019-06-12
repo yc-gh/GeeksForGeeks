60d8d69d07c09a613dd3801b26e06f7863240b99
YC <yashdipsite@gmail.com>
2019-06-12 19:02:27 +0530

    Initial commit

---
+++ Trees/Boundary traversal of binary tree.java
@@ -0,0 +1,74 @@
+//Print root node
+//Print left boundary nodes except leaf nodes
+//Print leaf nodes 
+//Print right boundary nodes except leaf nodes
+
+StringBuilder output;
+//Demonstrated initialization block
+{
+    output = new StringBuilder();
+}
+
+void printLeftBoundary(Node node)
+{
+    if(node==null) return;
+    //Traverse left while left child exists
+    if(node.left!=null)
+    {
+        //Print each node of left boundary
+        output.append(node.data+" ");
+        //Move to left child
+        printLeftBoundary(node.left);
+    }
+    //If right child exists, print and move right
+    else if(node.right!=null)
+    {
+        output.append(node.data+" ");
+        printLeftBoundary(node.right);
+    }
+    //Else if no children exist (leaf node is reached), do nothing
+}
+void printRightBoundary(Node node)
+{
+    if(node==null) return;
+    //Printing is done in reverse (after traversing)
+    //Traverse right while right child exists
+    if(node.right!=null)
+    {
+        printRightBoundary(node.right);
+        output.append(node.data+" ");
+    }
+    //If left child exists, move left
+    else if(node.left!=null)
+    {
+        printRightBoundary(node.left);
+        output.append(node.data+" ");
+    }
+    //Else if leaf node is reached, do nothing
+}
+void printLeaves(Node node)
+{
+    if(node==null) return;
+    //Inorder traversal, print leaf nodes
+    printLeaves(node.left);
+    if(node.left==null && node.right==null)
+    {
+        output.append(node.data+" ");
+    }
+    printLeaves(node.right);
+}
+void printBoundary(Node node)
+{
+    if(node==null) return;
+    output.append(node.data+" ");
+    //If methods are called with node as parameter, root node will get printed as well
+    //Print left boundary nodes
+    printLeftBoundary(node.left);
+    //Print leaf nodes of left subtree
+    printLeaves(node.left);
+    //Print leaf nodes of right subtree
+    printLeaves(node.right);
+    //Print right boundary nodes
+    printRightBoundary(node.right);
+    System.out.print(output);
+}
---
+++ Trees/Check for BST.java
@@ -0,0 +1,17 @@
+//Inefficient approach -- 
+//for each node check if left subtree's max value is less than node's data and right subtree's min value is greater than node's data
+
+//Efficient approach -- create a helper method that contains min and max values possible for current node
+//For each node, check if it is within that range
+//For it's left subtree, the range becomes previousmin <-> node.data-1
+//For it's right subtree, the range becomes node.data+1 <-> previousmax
+boolean isBSTUtil(Node root,int min,int max)
+{
+    if(root==null) return true;
+    if(root.data<min || root.data>max) return false;
+    return isBSTUtil(root.left,min,root.data-1) && isBSTUtil(root.right,root.data+1,max);
+}
+int isBST(Node root)  
+{
+    return isBSTUtil(root,Integer.MIN_VALUE,Integer.MAX_VALUE)==true ? 1 : 0;
+}
