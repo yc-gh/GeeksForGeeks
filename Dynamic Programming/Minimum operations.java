2bdb52751d746e165e554858b6626572bf2899c7
yc-gh <ycpcmr@gmail.com>
2020-05-18 10:53:23 +0530

    Updated files

--- Dynamic Programming/Minimum operations.java
+++ Dynamic Programming/Minimum operations.java
@@ -1,33 +1,24 @@
-    //Recursive method
-    static int findOperations(int n)
-    {
-        if(n==0 || n==1) return n;
-        if(n%2==0) return 1 + findOperations(n/2);
-        return 1 + findOperations(n-1);
-        
-    }
-	public static void main (String[] args) throws IOException
-	 {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    int t = Integer.parseInt(br.readLine().trim());
-	    while(t-->0)
-	    {
-	        int n = Integer.parseInt(br.readLine().trim());
-	        int count = 0;
-	        while(n>0)
-	        {
-	            if(n%2==0) 
-	            {
-	                n=n/2;
-	            }
-	            else
-	            {
-	                n--;
-	            }
-	            count++;
-	        }
-	        System.out.println(count);
-	       //For recursive solution
-	       // System.out.println(findOperations(n));
-	    }
-	 }
+// https://github.com/yc-gh/GeeksForGeeks
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine().trim());
+			int[] dp = new int[n + 1];
+			dp[1] = 1;
+			for (int i = 2; i <= n; i++) {
+				if ((i & 1) == 0)
+					dp[i] = dp[i >> 1] + 1;
+				else
+					dp[i] = dp[i - 1] + 1;
+			}
+			System.out.println(dp[n]);
+		}
+	}
+}
